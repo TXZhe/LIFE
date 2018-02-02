468de1a94d99bf450c2a8a31798a1d9e42e3536f
TXZhe <450111801@qq.com>
2018-02-03 02:31:03 +0800

    V0.02
    Add jump

--- Assets/Scripts/Player_controler.cs
+++ Assets/Scripts/Player_controler.cs
@@ -2,22 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class Player_controler : MonoBehaviour {
 
-    public float maxSpeed = 8f;
+    public float maxSpeed = 3.5f;
     bool facingRight = false;
-    Animator anim;
+    public Animator anim;
 
-	// Use this for initialization
-	void Start () {
+    public bool gameOver = false;
+
+    public bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700f;
+
+    // Use this for initialization
+    void Start () {
         anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        anim.SetBool("Grounded", grounded);
+
+        anim.SetFloat("vSpeed", GetComponent<Rigidbody2D>().velocity.y);
+
         float move = Input.GetAxis("Horizontal");
 
-        anim.SetFloat("speed", Mathf.Abs(move));
+        anim.SetFloat("Speed", Mathf.Abs(move));
+        anim.SetBool("GameOver", gameOver);
+
 
         GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
         if (move > 0 && !facingRight)
@@ -25,6 +42,15 @@ public class Player_controler : MonoBehaviour {
         else if (move < 0 && facingRight)
             Flip();
 	}
+
+    void Update()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            anim.SetBool("Grounded", false);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+        }
+    }
 
     void Flip(){
         facingRight = !facingRight;
